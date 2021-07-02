9e903c94acf33f4ec4347f4e8f18a410408c44cf
JackLong9106 <JackLong9106@outlook.com>
2021-07-02 18:10:23 +0100

    Added Material to shape

--- Shape.cs
+++ Shape.cs
@@ -7,15 +7,24 @@ namespace Raytracer2
 {
     abstract class Shape
     {
-        protected Color color;
+        protected Material material;
 
         public abstract bool CheckIntersection(Ray ray);
 
         public abstract Vector GetNormal();
+       
+        public double GetDiffuse()
+        {
+            return material.GetDiffuseIntensity();
+        }
 
+        public double GetSpecular()
+        {
+            return material.GetSpecularIntensity();
+        }
         public Color GetColor()
         {
-            return color;
+            return material.GetColor();
         }
     }
 }

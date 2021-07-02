3434fdefb3a162cfafc43634de0a11c84c1eed5f
JackLong9106 <JackLong9106@outlook.com>
2021-07-02 18:03:09 +0100

    Added Material class

---
+++ Material.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Raytracer2
+{
+    class Material
+    {
+        private double diffuseIntensity, specularIntensity;
+        Color colour;
+
+        public Material(double diffuse, double spec, Color colourIn)
+        {
+            diffuseIntensity = diffuse;
+            specularIntensity = spec;
+            colour = colourIn;
+        }
+
+        public double GetDiffuseIntensity()
+        {
+            return diffuseIntensity;
+        }
+
+        public double GetSpecularIntensity()
+        {
+            return specularIntensity;
+        }
+
+        public Color GetColor()
+        {
+            return colour;
+        }
+    }
+}
